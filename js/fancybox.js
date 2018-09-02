9cdf8de9a49ceb0fd8d2df305d38091b4efd2984
ZhuiMar <13670175092@163.com>
2018-09-02 22:34:29 +0800

    Site updated: 2018-09-02 22:34:29

--- js/fancybox.js
+++ js/fancybox.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     if ($(this).parent().hasClass('fancybox')) return;
     if ($(this).hasClass('nofancybox')) return;
     var alt = this.alt;
-    if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+    //if (alt) $(this).after('<span class="caption">' + alt + '</span>');
     $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" title="' + alt + '" class="fancybox"></a>');
   });
   $(this).find('.fancybox').each(function(){
--- js/totop.js
+++ js/totop.js
@@ -1,12 +1,35 @@
-$(window).scroll(function() {
-    $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
-});
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 500, function() {
-        $("#rocket").removeClass("show launch");
-    });
-    return false;
+function myEvent(obj,ev,fn){
+	if(obj.attachEvent){
+		obj.attachEvent('on'+ev,fn);
+	}else{
+		obj.addEventListener(ev,fn,false);
+	}
+}
+myEvent(window,'load',function(){
+	var oRTT=document.getElementById('back_to_top');
+	var pH=document.documentElement.clientHeight;
+	var timer=null;
+	var scrollTop;
+	window.onscroll=function(){
+		scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
+		if(scrollTop>=pH){
+			oRTT.style.display='block';
+		}else{
+			oRTT.style.display='none';
+		}
+		return scrollTop;
+	};
+	oRTT.onclick=function(){
+		clearInterval(timer);
+		timer=setInterval(function(){
+			var now=scrollTop;
+			var speed=(0-now)/10;
+			speed=speed>0?Math.ceil(speed):Math.floor(speed);
+			if(scrollTop==0){
+				clearInterval(timer);
+			}
+			document.documentElement.scrollTop=scrollTop+speed;
+			document.body.scrollTop=scrollTop+speed;
+		}, 30);
+	}
 });
